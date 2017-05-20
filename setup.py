cf70c544babf1536490e7902b6d14c67a0a96497
Dash Winterson <dashdanw@Dashs-MacBook-Pro.local>
2017-05-20 01:56:52 -0400

    added setup for package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='django-htcpcp',
+  packages=['django-htcpcp'],
+  version='0.1.0',
+  description='Django HyperText Coffee Pot Protocol Middleware (HTCPCP)',
+  author='Dash Winterson',
+  author_email='dashdanw@gmail.com',
+  url='https://github.com/dashdanw/django-htcpcp',
+  download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+  keywords=['htcpcp', 'django', 'middleware', 'RFC 2324'],
+  classifiers=[],
+)
