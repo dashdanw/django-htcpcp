2c33390762a989abf8683e906923e3e739bf8e22
Dash Winterson <dashdanw@Dashs-MacBook-Pro.local>
2017-05-20 02:09:59 -0400

    changed setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author='Dash Winterson',
   author_email='dashdanw@gmail.com',
   url='https://github.com/dashdanw/django-htcpcp',
-  download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+  download_url='https://github.com/dashdanw/django-htcpcp/archive/0.1.0.tar.gz',
   keywords=['htcpcp', 'django', 'middleware', 'RFC 2324'],
   classifiers=[],
 )
