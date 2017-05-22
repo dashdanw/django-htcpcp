012b2f8c8aea4e77e229687838c493ad4b53e8b5
Dash Winterson <dashdanw@Dashs-MacBook-Pro.local>
2017-05-22 16:29:36 -0400

    updated module

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 from distutils.core import setup
+import djhtcpcp
+
 setup(
   name='django-htcpcp',
   packages=[
-    'djhtcpcp',
-    'djhtcpcp.middleware'
+    'djhtcpcp'
    ],
   install_requires=[
         'setuptools',
         'Django>=1.8'
    ],
-  version='0.1.3',
+  version=djhtcpcp.__version__,
   description='Django HyperText Coffee Pot Protocol Middleware (HTCPCP)',
   author='Dash Winterson',
   author_email='dashdanw@gmail.com',
   url='https://github.com/dashdanw/django-htcpcp',
-  download_url='https://github.com/dashdanw/django-htcpcp/archive/0.1.1.tar.gz',
-  keywords=['htcpcp', 'django', 'middleware', 'RFC 2324'],
+  download_url='https://github.com/dashdanw/django-htcpcp/archive/{}.tar.gz'.format(djhtcpcp.__version__),
+  keywords=[
+    'htcpcp',
+    'django',
+    'middleware',
+    'RFC 2324',
+    'coffee'
+  ],
   classifiers=[],
 )
