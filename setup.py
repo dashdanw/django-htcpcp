e67755f0d037420bcc3801c01003d4ae197fc6de
Dash Winterson <dashdanw@Dashs-MacBook-Pro.local>
2017-05-26 23:24:49 -0400

    updated to version 0.2.5

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 from distutils.core import setup
 import djhtcpcp
+import pypandoc
 
 setup(
-  name='django-htcpcp',
-  packages=[
-    'djhtcpcp'
-   ],
-  install_requires=[
-        'setuptools',
-        'Django>=1.8'
-   ],
-  version=djhtcpcp.__version__,
-  description='Django HyperText Coffee Pot Protocol Middleware (HTCPCP)',
-  author='Dash Winterson',
-  author_email='dashdanw@gmail.com',
-  url='https://github.com/dashdanw/django-htcpcp',
-  download_url='https://github.com/dashdanw/django-htcpcp/archive/{}.tar.gz'.format(djhtcpcp.__version__),
-  keywords=[
-    'htcpcp',
-    'django',
-    'middleware',
-    'RFC 2324',
-    'coffee'
-  ],
-  classifiers=[],
+    name='django-htcpcp',
+    packages=[
+      'djhtcpcp'
+     ],
+    install_requires=[
+          'setuptools',
+          'Django>=1.8'
+     ],
+    version=djhtcpcp.__version__,
+    description='Django HyperText Coffee Pot Protocol Middleware (HTCPCP)',
+    author='Dash Winterson',
+    author_email='dashdanw@gmail.com',
+    url='https://github.com/dashdanw/django-htcpcp',
+    download_url='https://github.com/dashdanw/django-htcpcp/archive/{}.tar.gz'.format(djhtcpcp.__version__),
+    keywords=[
+      'htcpcp',
+      'django',
+      'middleware',
+      'RFC 2324',
+      'coffee'
+    ],
+    classifiers=[],
+    long_description=pypandoc.convert('README.md', 'rst')
 )
